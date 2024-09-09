2367cacf061e71c60f16bda17b0fffac6eb3a2b1
multiahfoon <multiahfoon@gmail.com>
2024-09-10 10:11:50 +1200

    refactor: remove code not being used

--- app/store/authStore.tsx
+++ app/store/authStore.tsx
@@ -1,25 +1,16 @@
-import { create } from "zustand";
+import { create } from 'zustand'
 
 interface UserState {
-  user: { uid: string; email: string; pass: string } | null;
-  setUser: (user: { uid: string; email: string; pass: string }) => void;
-  clearUser: () => void;
+	user: { uid: string; email: string; pass: string } | null
+	setUser: (user: { uid: string; email: string; pass: string }) => void
+	clearUser: () => void
 }
 
 // Create the Zustand store
 const useUserStore = create<UserState>((set) => ({
-  user: null, // Initial state
-  setUser: (user) => set({ user }), // Set user data
-  clearUser: () => set({ user: null }), // Clear user data
-}));
+	user: null, // Initial state
+	setUser: (user) => set({ user }), // Set user data
+	clearUser: () => set({ user: null }), // Clear user data
+}))
 
-export default useUserStore;
-
-// when you reflesh user become null initially so you need to fetch info from local storage
-// const useAuthStore = create((set)=>({
-//     user:null,
-//     login:(user)=>{set({user})},
-//     logout:()=>set({user:null}),
-//     setUser:(user)=>set({user})
-
-// }))
+export default useUserStore
