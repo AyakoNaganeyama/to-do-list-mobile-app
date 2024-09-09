562d518499137d0054f71ae868fb1a69d7ac93c4
multiahfoon <multiahfoon@gmail.com>
2024-09-10 10:14:11 +1200

    refactor: remove getStorage not being used

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,26 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-import {getStorage} from "firebase/storage";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDYmPOhGG7eIF4wEFG26WMXT6NVI96hiAw",
-    authDomain: "to-do-app-90ad5.firebaseapp.com",
-    projectId: "to-do-app-90ad5",
-    storageBucket: "to-do-app-90ad5.appspot.com",
-    messagingSenderId: "524745754394",
-    appId: "1:524745754394:web:9de372fce572fe538a4ea3"
-};
+	apiKey: 'AIzaSyDYmPOhGG7eIF4wEFG26WMXT6NVI96hiAw',
+	authDomain: 'to-do-app-90ad5.firebaseapp.com',
+	projectId: 'to-do-app-90ad5',
+	storageBucket: 'to-do-app-90ad5.appspot.com',
+	messagingSenderId: '524745754394',
+	appId: '1:524745754394:web:9de372fce572fe538a4ea3',
+}
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
 // Initialize Firestore
-const database = getFirestore(app);
+const database = getFirestore(app)
 
 // Initialize Firebase Authentication
-const fireAuth = getAuth(app);
+const fireAuth = getAuth(app)
 
 // Export the initialized instances
-export { app, fireAuth, database };
+export { app, fireAuth, database }
