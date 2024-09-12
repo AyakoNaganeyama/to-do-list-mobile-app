e13d207a62accf6a8aa8fd94869790f34b4fa84f
multiahfoon <multiahfoon@gmail.com>
2024-09-12 21:37:33 +1200

    feat: remove toaster logic out of login hook

--- app/hooks/useLogin.tsx
+++ app/hooks/useLogin.tsx
@@ -1,7 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
-
 import useUserStore from '../store/authStore'
-import { useToast } from '../hooks/useToast'
 
 // Define the User type
 interface User {
@@ -11,44 +9,27 @@ interface User {
 }
 
 export function useLogin() {
-	const { showSuccessToast, showErrorToast } = useToast()
-
-	const globalLogin = useUserStore((state) => state.setUser)
-	const globalUser = useUserStore((state) => state.user)
+	// make sure setting and getting same instance
+	const { setUser: globalLogin, user: globalUser } = useUserStore((state) => ({
+		setUser: state.setUser,
+		user: state.user,
+	}))
 
 	async function login(email: string, password: string) {
-		try {
-			const jsonValue = await AsyncStorage.getItem('users') // get user data from async storage
-			if (jsonValue !== null) {
-				const users: User[] = JSON.parse(jsonValue) //covert json string into array of user objects
-				// Find the user that matches both email and password
-				const foundUser = users.find(
-					(user) => user.email === email && user.pass === password
-				)
-
-				if (foundUser) {
-					//if fount show success toast
-					showSuccessToast(
-						'Login Successful',
-						`Welcome back, ${foundUser.email}!`
-					)
-
-					//set found user in zustand for global access
-					globalLogin(foundUser)
-
-					//this is for testing
-					console.log('zustand', globalUser)
-				} else {
-					showErrorToast('Login Failed', 'Invalid email or password.')
-					console.log('Login Failed', 'Invalid email or password.')
-				}
-			}
-		} catch (error) {
-			console.error('Error during login:', error)
-			showErrorToast(
-				'Login Error',
-				'An error occurred during login. Please try again.'
+		const jsonValue = await AsyncStorage.getItem('users') // get user data from async storage
+		if (jsonValue !== null) {
+			const users: User[] = JSON.parse(jsonValue) //covert json string into array of user objects
+			// Find the user that matches both email and password
+			const foundUser = users.find(
+				(user) => user.email === email && user.pass === password
 			)
+
+			if (foundUser) {
+				//set found user in zustand for global access
+				globalLogin(foundUser)
+			} else {
+				console.log('Login Failed', 'Invalid email or password.')
+			}
 		}
 	}
 
