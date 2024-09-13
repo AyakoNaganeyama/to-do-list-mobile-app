def1e938ef9566d9af3a111084c8052779f0cc91
multiahfoon <multiahfoon@gmail.com>
2024-09-13 12:34:49 +1200

    refactor: remove toast from useLogout
    Toast notification will be moved to app component

--- app/hooks/useLogout.tsx
+++ app/hooks/useLogout.tsx
@@ -1,14 +1,11 @@
 import useUserStore from '../store/authStore'
-import { useToast } from '../hooks/useToast'
 
 export function useLogout() {
 	const globalLogout = useUserStore((state) => state.clearUser)
-	const { showSuccessToast } = useToast()
 
 	function logout() {
 		//set zustand use to null
 		globalLogout()
-		showSuccessToast('Logout Successful', 'You have been logged out.')
 	}
 
 	return { logout }
